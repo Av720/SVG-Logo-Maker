a9fd1dda616c2600b120478cc332cd4962270921
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-06 23:12:26 -0700

    added index file and main variables for require

---
+++ index.js
@@ -0,0 +1,7 @@
+// create a variable for the filesystem  and use require
+// create a varibale for inquirer and use require
+//create a variable for the shapes 
+
+const mainFile = require()
+const inquirer = require("inquirer")
+const {Square, Triangle, Circle} = require("./lib/shapes")
