38d34b7992768bcee2497e472d57e61bda152dd1
Av720 <120581801+Av720@users.noreply.github.com>
2023-03-07 00:00:06 -0700

    added  class SVG constructor and the 2 keys associated

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // create a variable for the filesystem  and use require
 // create a varibale for inquirer and use require
-//create a variable for the shapes 
+//create a variable for the shapes
+// create a class for the SVG file
 
-const mainFile = require()
-const inquirer = require("inquirer")
-const {Square, Triangle, Circle} = require("./lib/shapes")
+const mainFile = require();
+const inquirer = require("inquirer");
+const { Square, Triangle, Circle } = require("./lib/shapes");
+
+class Svg {
+  constructor() {
+    this.text = "";
+    this.shape = "";
+  }
+  render() {
+    return ``
+  }
+}
